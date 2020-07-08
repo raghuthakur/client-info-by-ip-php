f66cd0688ad4696e3890bfb07b86bfaf84ee5886
Ashutosh <ashutosh@tecuho.com>
2020-07-08 16:21:47 +0530

    first commit

---
+++ getIp.php
@@ -0,0 +1,32 @@
+<?php
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+
+
+
+$ipdat = @json_decode(file_get_contents( 
+    "http://www.geoplugin.net/json.gp?ip=" . $ipaddress)); 
+
+
+   return $ipdat;
+}
+
+print_r(get_client_ip());
+
+
+
+?>
